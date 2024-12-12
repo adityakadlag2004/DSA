7c500236699ec7df31e43020c0ef937749a9e01a
Aditya Kadlag <adityakadlag2004@gmail.com>
2024-12-12 19:40:27 +0530

    `Added comments to hashusingmap.cpp explaining map properties`

--- hashing/hashusingmap.cpp
+++ hashing/hashusingmap.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 #include<map>
 using namespace std;
+
+//map properties
+//1. map is a container in C++ that stores elements in a key-value pair.
+//2. map is an associative container, meaning that it stores elements in a way that they can
+//   be accessed by a key.
+//3. map is a sorted container, meaning that the elements are stored in a sorted order.
+//4. map is a dynamic container, meaning that it can grow or shrink as elements are added
 int main()
 {
     int n;
