6e686707b76c01e14cd14196f71b8dec87468da1
Aditya Kadlag <adityakadlag2004@gmail.com>
2025-02-05 19:06:54 +0530

    Commit 5

--- sorts/quick.cpp
+++ sorts/quick.cpp
@@ -1,43 +1,51 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int partition(int arr[],int low,int high)
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int arr[], int low, int high)
 {
-    int pivot = arr[low];
-    int i=low,j=high;
-    while(i<=j){
-        while(arr[i]<arr[pivot] && i <= high){
+    int pivot = low;
+    int i = low, j = high;
+    while (i < j)
+    {
+        while (arr[i] <= arr[pivot] && i <= high-1)
+        {
             i++;
         }
-        while(arr[j]>arr[pivot] && j >= low){
+        while (arr[j] >= arr[pivot] && j > low)
+        {
             j--;
         }
-       int temp = arr[i];
-       arr[i] = arr[j];
-       arr[j] = temp;
+        if (i < j) swap(arr[i],arr[j]);
     }
-    int temp = arr[low];
-    arr[low] = arr[j];
-    arr[j] = temp;
+    swap(arr[low],arr[j]);
     return j;
 }
-void QuickSort(int arr[],int low,int high)
+void QuickSort(int arr[], int low, int high)
 {
-    if(low<high){
-        int pivot=partition(arr,low,high);
-        QuickSort(arr,low,pivot-1);
-        QuickSort(arr,pivot+1,high);
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        QuickSort(arr, low, pivot - 1);
+        QuickSort(arr, pivot + 1, high);
     }
 }
-int main(){
-    //unsorted Array
+int main()
+{
+    // unsorted Array
     int arr[] = {5, 2, 8, 1, 9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    QuickSort(arr,0,n-1);
-    cout<<"Sorted array is: ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-        }
-        return 0;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    QuickSort(arr, 0, n - 1);
+    cout << "Sorted array is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
